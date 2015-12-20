48e7d976ac8a9a1a1450a10b805fec42a496f054
ing_mig21 <yher136@gmail.com>
2015-12-20 16:56:50 -0430

    Nuevo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 	end
 
 	def new		
+		@post = Post.new
 	end
 
 	def show
@@ -13,9 +14,28 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
+		
+		if @post.save
 
-		redirect_to @post
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+
+		
 	end
 
 	private
